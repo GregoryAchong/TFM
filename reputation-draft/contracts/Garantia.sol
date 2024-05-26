4173103a442a03c408f732c521900859b6658873
Gregory Achong <47358424+GregoryAchong@users.noreply.github.com>
2024-05-26 15:40:14 +0200

    Update Garantia.sol

--- reputation-draft/contracts/Garantia.sol
+++ reputation-draft/contracts/Garantia.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.0;
 import './Reputation.sol';
 import './PagoAlquiler.sol';
 
-//esta es la garantia extra si no tiene reputacion. 
 contract Garantia {
 
 address owner;
 address user;
 int256 amount;
 bool isBlocked;
-uint256 _LiberarGarantia = block.timestamp + 180 days;
+uint256 LiberarGarantia;
 
  constructor() {
         // Set the transaction sender as the owner of the contract.
@@ -23,24 +22,26 @@ _;
 }
 
  function retenerGarantia  ( address _user , int256 _amount) public  onlyOwner {
-    
+    //se toma de pago alquiles el dia de inicio del pago 
+    PagoAlquiler AlquilerPago;
+    //se suman los 6 meses para saber la fecha de devolucion 
+    LiberarGarantia = AlquilerPago.enviarInicioPago() +180 days;
     amount = _amount;
     user=_user;
+    //se bloquea en este contrato el poder hacer
     isBlocked=true;
  } 
   
-
- function liberarGarantia (uint256 _tiempoTranscurrido , address payable  withdrawAddress, uint _amount) public  onlyOwner  {
-   
-   if( _tiempoTranscurrido == _LiberarGarantia && isBlocked == true  ){
+ function garantiaLiberada (uint256 _tiempoTranscurrido , address payable  withdrawAddress, uint _amount) public  onlyOwner  {
+   //se compara si el tiempo transcurrido es mayor o igual a 6meses y si la cuenta sigue bloqueada 
+   if( _tiempoTranscurrido >= LiberarGarantia && isBlocked == true ){
+      //se desbloquea la cuenta 
        isBlocked=false;
+       // se envian los fondos bloqueado
        withdrawAddress.transfer(_amount);
   }
-
-   
 
    }
    
  } 
-
 
