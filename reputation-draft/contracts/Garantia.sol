abad3312e11b56e9442d4007bcef901ae8cbfec0
Gregory Achong <47358424+GregoryAchong@users.noreply.github.com>
2024-05-26 15:20:13 +0200

    Update Garantia.sol

--- reputation-draft/contracts/Garantia.sol
+++ reputation-draft/contracts/Garantia.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import './Reputation.sol';
@@ -7,8 +6,10 @@ import './PagoAlquiler.sol';
 
 contract Garantia {
 
-
 address owner;
+address user;
+int256 amount;
+bool isBlocked;
 uint256 _LiberarGarantia = block.timestamp + 180 days;
 
  constructor() {
@@ -16,21 +17,30 @@ uint256 _LiberarGarantia = block.timestamp + 180 days;
         owner = msg.sender;
     }
 
-
 modifier onlyOwner {
 require(msg.sender == owner );
 _;
 }
 
- function retenerGarantia  () public  onlyOwner {
-
+ function retenerGarantia  ( address _user , int256 _amount) public  onlyOwner {
+    
+    amount = _amount;
+    user=_user;
+    isBlocked=true;
  } 
+  
 
- function liberarGarantia (uint256 _tiempoTranscurrido) public view onlyOwner  {
+ function liberarGarantia (uint256 _tiempoTranscurrido , address payable  withdrawAddress, uint _amount) public  onlyOwner  {
    
-   if( _tiempoTranscurrido == _LiberarGarantia ){
+   if( _tiempoTranscurrido == _LiberarGarantia  ){
+       isBlocked=false;
+       withdrawAddress.transfer(_amount);
+  }
 
-
+   
 
    }
    
+ } 
+
+
